0471c2038837efbabea367ffbf3d9c6294db41b3
h-zahar <hz.rifat@gmail.com>
2025-07-30 23:26:19 +0600

    brute force two sum

---
+++ two-sum.js
@@ -0,0 +1,15 @@
+const getTwoSum = (arr, target) => {
+	for (let i = 0; i < arr.length; i++) {
+		let p1 = arr[i];
+		let newTarget = target - p1;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] === newTarget) {
+				return [i, j];
+			}
+		}
+	}
+}
+
+const indexes = getTwoSum([1, 3, 7, 9, 2], 11);
+
+console.log(indexes);
