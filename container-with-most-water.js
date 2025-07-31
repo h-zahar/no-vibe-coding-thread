3e94bd343df42b428c4c2142c667e35629fad65a
h-zahar <hz.rifat@gmail.com>
2025-07-31 22:58:25 +0600

    optimized with shifting pointers

--- container-with-most-water.js
+++ container-with-most-water.js
@@ -16,3 +16,31 @@ const getAreaWithMostWater = (arr) => {
 const areaWithMostWater = getAreaWithMostWater([7, 1, 2, 3, 9]); 
 
 console.log(areaWithMostWater);
+
+
+
+// shifting pointers
+
+const getAreaWithMostWaterOpt = (arr) => {
+	let maxArea = 0;
+	let p1 = 0;
+	let p2 = arr.length - 1;
+		
+	while (p2 - p1) {
+		const objHeight = Math.min(arr[p1], arr[p2]);
+		const objWidth = (p2 - p1);
+		const area = objWidth * objHeight;
+		maxArea = Math.max(area, maxArea);
+		if (arr[p1] <= arr[p2]) {
+			p1++;
+		} else if (arr[p2] < arr[p1]) {
+			p2--;
+		}
+	}	
+
+	return maxArea;
+};
+
+const areaWithMostWaterOpt = getAreaWithMostWaterOpt([1,8,6,2,5,4,8,3,7]); 
+
+console.log(areaWithMostWaterOpt);
