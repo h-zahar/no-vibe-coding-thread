9c1a60c0b2827a99814e40ffcdadf8eb90d9487b
h-zahar <hz.rifat@gmail.com>
2025-07-31 20:34:04 +0600

    escaped unnecessary storage

--- container-with-most-water.js
+++ container-with-most-water.js
@@ -1,16 +1,18 @@
 // brute force
 
 const getAreaWithMostWater = (arr) => {
-	const listOfAreas = [0];
+	let maxArea = 0;
 	for (let i = 0; i < arr.length; i++) {
 		let p1 = i;
 		for (let j = i + 1; j < arr.length; j++) {
 			let p2 = j;
 			let area = Math.min(arr[p1], arr[p2]) * (p2 - p1);
-			listOfAreas.push(area);
+			if (area > maxArea) {
+				maxArea = area;
+			}
 		}
 	}
-	return Math.max(...listOfAreas);
+	return maxArea;
 };
 
 const areaWithMostWater = getAreaWithMostWater([7, 1, 2, 3, 9]); 
