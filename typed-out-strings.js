cbbede07a6018ad56408624a8378a298e91a6b24
h-zahar <hz.rifat@gmail.com>
2025-08-03 00:20:30 +0600

    brute forced typed out string

--- typed-out-strings.js
+++ typed-out-strings.js
@@ -1,11 +1,24 @@
 // brute force
 
-const getIsEqual = (string1, stirng2) => {
+const getIsEqualStrs = (string1, string2) => {
 	const getTypedOutFormat = (rawFormStr) => {
-
+		let typedStrArr = [];
+		const strArr = rawFormStr.split('');
+		for (let elem of strArr) {
+			if (elem === '#') {
+				typedStrArr.pop();
+			} else {
+				typedStrArr.push(elem);
+			}
+		}
+		return typedStrArr.join('');
 	};
 	const typedFormStr1 = getTypedOutFormat(string1);
 	const typedFormStr2 = getTypedOutFormat(string2);
 	
-	return tyepdFormStr1 === typedFormStr2;
+	return typedFormStr1 === typedFormStr2;
 };
+
+const isEqualStrs = getIsEqualStrs('ab#z', 'az#z');
+
+console.log(isEqualStrs);
