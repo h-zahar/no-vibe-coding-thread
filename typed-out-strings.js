a15557643a680f30e05942973f87ec0c34b9bac0
h-zahar <hz.rifat@gmail.com>
2025-08-03 01:07:11 +0600

    optimized space

--- typed-out-strings.js
+++ typed-out-strings.js
@@ -19,6 +19,40 @@ const getIsEqualStrs = (string1, string2) => {
 	return typedFormStr1 === typedFormStr2;
 };
 
-const isEqualStrs = getIsEqualStrs('ab#z', 'az#z');
+const isEqualStrs = getIsEqualStrs('ab##', 'c#d#');
 
 console.log(isEqualStrs);
+
+
+
+// optimal - space complexity-wise
+
+const getIsEqualStrsOp = (string1, string2) => {
+	const getTypedOutFormatOp = (rawFormStr) => {
+		const strArr = rawFormStr.split('');
+		let p = strArr.length - 1;
+		let toRemove = 0;
+		
+		while (p >= 0) {
+			if (strArr[p] !== '#') {
+				if (toRemove) {
+					strArr.splice(p, 1);
+					toRemove--;
+				}
+			} else if (strArr[p] === '#') {
+				toRemove++;
+				strArr.splice(p, 1);
+			}
+			p--;	
+		}		
+		return strArr.join('');
+	};
+	const typedFormOpStr1 = getTypedOutFormatOp(string1);
+	const typedFormOpStr2 = getTypedOutFormatOp(string2);
+	
+	return typedFormOpStr1 === typedFormOpStr2;
+};
+
+const isEqualStrsOp = getIsEqualStrsOp('ab##', 'c#d#');
+
+console.log(isEqualStrsOp);
