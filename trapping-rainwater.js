4bbddad37d431446b91678ddb6550c95ac1a506c
h-zahar <hz.rifat@gmail.com>
2025-08-02 23:44:05 +0600

    added two pointers

--- trapping-rainwater.js
+++ trapping-rainwater.js
@@ -25,3 +25,41 @@ const testCase = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 const trappedWater = getTrappedWater(testCase);
 
 console.log(trappedWater);
+
+
+
+// two pointer
+
+const getTrappedWaterOp = (arr) => {
+	let totalWater = 0;
+	let p1 = 0;
+	let p2 = arr.length - 1;
+	let maxL = 0;
+	let maxR = 0;
+	
+	while (p2 - p1) {
+		if (arr[p1] <= arr[p2]) {
+			if (arr[p1] < maxL) {
+				totalWater += (maxL - arr[p1]);
+			} else {
+				maxL = arr[p1];
+			}
+			p1++;
+		} else if (arr[p2] < arr[p1]) {
+			if (arr[p2] < maxR) {
+				totalWater += (maxR - arr[p2]);
+			} else {
+				maxR = arr[p2];
+			}
+			p2--;
+		}
+	}
+
+	return totalWater;
+};
+
+const testCaseOp = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+
+const trappedWaterOp = getTrappedWaterOp(testCaseOp);
+
+console.log(trappedWaterOp);
