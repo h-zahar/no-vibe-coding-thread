4b606244a7923f0b77316f99a33e4d0694bc970a
h-zahar <hz.rifat@gmail.com>
2025-08-03 01:30:18 +0600

    added bruteforce sol

--- longest-substring.js
+++ longest-substring.js
@@ -1,7 +1,26 @@
 // brute-force
 
-const getLongestNonRepetedSubstring = (string) => {
-	
+const getLongestNonRepeatedSubstring = (string) => {
+	let longest = 0;
+	const strArr = string.split('');
+	for (let i = 0; i < strArr.length; i++) {
+		const memory = {};
+		let count = 0;
+		for (let j = i; j < strArr.length; j++) {
+			if (memory?.[strArr[j]]) {
+				if (count > longest) {
+					longest = count;
+				}
+				break;
+			} else {
+				memory[strArr[j]] = 1;
+				count++;
+			}
+		}
+	}
+	return longest;
 };
 
-const longest = getLongestNonRepeatedSubstring('abcbdca'); 
+const longestStrCount = getLongestNonRepeatedSubstring('abcbdca');
+
+console.log(longestStrCount);
