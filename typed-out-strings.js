e7ee1c33c106caa1c2c6522fdb4dd861fe2690e9
h-zahar <hz.rifat@gmail.com>
2025-08-03 00:00:42 +0600

    set up skeleton

---
+++ typed-out-strings.js
@@ -0,0 +1,11 @@
+// brute force
+
+const getIsEqual = (string1, stirng2) => {
+	const getTypedOutFormat = (rawFormStr) => {
+
+	};
+	const typedFormStr1 = getTypedOutFormat(string1);
+	const typedFormStr2 = getTypedOutFormat(string2);
+	
+	return tyepdFormStr1 === typedFormStr2;
+};
