c0f1da8fedfab5c65b0429eac2f915a2c8653050
h-zahar <hz.rifat@gmail.com>
2025-08-05 23:42:42 +0600

    added sliding window

--- longest-substring.js
+++ longest-substring.js
@@ -7,20 +7,53 @@ const getLongestNonRepeatedSubstring = (string) => {
 		const memory = {};
 		let count = 0;
 		for (let j = i; j < strArr.length; j++) {
+			if (count > longest) {
+           	longest = count;
+           }
 			if (memory?.[strArr[j]]) {
 				break;
 			} else {
 				memory[strArr[j]] = 1;
 				count++;
 			}
-			if (count > longest) {
-           	longest = count;
-           }
 		}
 	}
 	return longest;
 };
 
-const longestStrCount = getLongestNonRepeatedSubstring('abcbdca');
+const longestStrCount = getLongestNonRepeatedSubstring('abcabcbb');
 
 console.log(longestStrCount);
+
+
+
+// sliding window
+
+const getLongestNonRepeatedSubstringSW = (string) => {
+	if (string.length <= 1) {
+		return string.length;
+	}
+	let longest = 0;
+	const strArr = string.split('');
+	const memory = {};
+	memory[strArr[0]] = 0;
+	let lW = 0;
+	let rW = 1;
+
+	while (rW <= strArr.length) {
+		if (rW - lW > longest) {
+			longest = rW - lW;
+		}
+		if (memory?.[strArr[rW]] !== undefined && memory?.[strArr[rW]] >= lW) {
+			lW = memory[strArr[rW]] + 1;
+		}
+		memory[strArr[rW]] = rW;
+		rW++;
+	}
+
+	return longest;
+};
+
+const longestStrCountSW = getLongestNonRepeatedSubstringSW('au');
+
+console.log(longestStrCountSW);
