7d0e5b61e81f3b06dac511304a1a9cc84d160370
h-zahar <hz.rifat@gmail.com>
2025-08-06 01:17:01 +0600

    added bruteforce

--- almost-a-palindrome.js
+++ almost-a-palindrome.js
@@ -1,7 +1,24 @@
-const isAlmostPalindrome = () => {
-	
+// brute force solution
+
+const isAlmostPalindrome = (string) => {
+	const originalArr = string.replace('/[^a-zA-Z0-9]/g', '').split('');
+	let p = 0;
+	let isFound = false;
+
+	for (let p = 0; p < originalArr.length; p++) {
+		const tempArr = [...originalArr];
+		tempArr.splice(p, 1);
+		const newArr = [...tempArr];
+		const compareArr = [...newArr].reverse();
+		if (newArr.join('') === compareArr.join('')) {
+			isFound = true;
+			break;
+		}
+	}
+
+	return isFound;
 };
 
-const isValid = isAlmostPalindrome('a cats dog');
+const isValid = isAlmostPalindrome('race a car');
 
 console.log(isValid);
