70044417a08fff752ae04fb6ec84e77945adfb2d
h-zahar <hz.rifat@gmail.com>
2026-03-11 03:31:59 +0600

    stacked a problem

---
+++ 4-stack/1-valid-parentheses.py
@@ -0,0 +1,37 @@
+"""
+Valid Parentheses
+
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise.
+"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        newst = []
+
+        for t in s:
+            if t in [")", "}", "]"]:
+                if not newst:
+                    return False
+
+            if t in [")", "}", "]"]:
+                if t == ")" and newst[-1] == "(":
+                    newst.pop()
+                elif t == "}" and newst[-1] == "{":
+                    newst.pop()
+                elif t == "]" and newst[-1] == "[":
+                    newst.pop()
+                else:
+                    return False
+
+            elif t in ["(", "{", "["]:
+                newst.append(t)
+        
+        return True if len(newst) == 0 else False
+            
