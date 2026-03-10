de1b74767821bbe0db4274377815da27960b65dd
h-zahar <hz.rifat@gmail.com>
2026-03-11 03:20:38 +0600

    solved a problem using two pointers technique

---
+++ 2-two-pointers/4-best-time-buy-sell-stock.py
@@ -0,0 +1,25 @@
+"""
+Best Time to Buy and Sell Stock
+
+You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+
+You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        lp = 0
+        rp = lp + 1
+        max_profit = 0
+
+        while rp < len(prices):
+            profit = prices[rp] - prices[lp]
+            max_profit = max(profit, max_profit)
+            if prices[lp] >= prices[rp]:
+                lp = rp
+                rp += 1
+            else:
+                rp += 1
+        return max_profit
