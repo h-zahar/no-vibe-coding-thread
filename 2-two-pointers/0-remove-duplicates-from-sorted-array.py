4a2167cd0dd3911cd7a5aaf164f8780d2d49a022
h-zahar <hz.rifat@gmail.com>
2026-04-01 04:59:40 +0600

    added a two pointer easy problem

---
+++ 2-two-pointers/0-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,26 @@
+"""
+Remove Duplicates From Sorted Array
+Easy
+Topics
+Company Tags
+You are given an integer array nums sorted in non-decreasing order. Your task is to remove duplicates from nums in-place so that each element appears only once.
+
+After removing the duplicates, return the number of unique elements, denoted as k, such that the first k elements of nums contain the unique elements.
+
+Note:
+
+The order of the unique elements should remain the same as in the original array.
+It is not necessary to consider elements beyond the first k positions of the array.
+To be accepted, the first k elements of nums must contain all the unique elements.
+Return k as the final result.
+"""
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+        for i in range(len(nums) - 1):
+            if nums[k] != nums[i + 1]:
+                k += 1
+            nums[k] = nums[i + 1]
+
+        return k + 1
